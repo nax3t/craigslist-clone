61fd7aae754344b2cd83bd1231a378c18d18f28a
Ian Schoonover <iantskon@gmail.com>
2016-10-15 20:34:12 -0500

    Add Post new form and show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,29 @@ class PostsController < ApplicationController
   end
 
   def new
+  	@post = Post.new
+  end
+
+  def create
+  	@post = Post.new(post_params)
+  	if @post.save
+	  	redirect_to @post
+	  else
+	  	render :new
+	  end
   end
 
   def show
+  	@post = Post.find(params[:id])
   end
 
   def edit
   end
+	
+	private
+	
+	def post_params
+		params.require(:post).permit(:title, :cost, :body, :image)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-
-  get 'posts/new'
-
-  get 'posts/show'
-
-  get 'posts/edit'
-
+	resources :posts
+	
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
